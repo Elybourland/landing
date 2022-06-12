7e2b8111245b7444d0b36dfc88f701cf97dddac1
Elybourland <JEBourland@yahoo.com>
2022-06-11 22:23:51 -0700

    transforming coords to query params

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +9,19 @@ import { Observable } from 'rxjs';
 export class ForecastService {
 
   constructor() { }
+
+  getForecast() {
+    return this.getCurrentLocation()
+    .pipe(
+      map(coords => {
+        return new HttpParams()
+          .set('lat', String(coords.latitude))
+          .set('lon', String(coords.longitude))
+          .set('units', 'metric')
+          .set('appid', '5c64c2dee682e7e661f7a01c93b8db6e')
+      })
+    )
+  }
 
   getCurrentLocation() {
     return new Observable<GeolocationCoordinates>((observer) => {
